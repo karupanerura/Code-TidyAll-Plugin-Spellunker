e35a58557ad9f07fa003e542f7cfd242d5f2b85f
karupanerura <karupa@cpan.org>
2019-06-20 12:17:06 -0400

    tweaks

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # NAME
 
-Code::TidyAll::Plugin::Spellunker - It's new $module
+Code::TidyAll::Plugin::Spellunker - Code::TydyAll plugin for Spellunker
 
 # SYNOPSIS
 
-    use Code::TidyAll::Plugin::Spellunker;
+    [Spellunker]
+    select = doc/**/*.txt
+    stopwords = karupanerura
+
+    [Spellunker::Pod]
+    select = lib/**/*.{pm,pod}
+    stopwords = karupanerura
 
 # DESCRIPTION
 
-Code::TidyAll::Plugin::Spellunker is ...
+Code::TidyAll::Plugin::Spellunker is Code::TydyAll plugin for Spellunker.
+
+# OPTIONS
+
+## stopwords
+
+Add stopwords to the on memory dictionary. Separate it by ",".
+
+SEE ALSO: https://metacpan.org/pod/Spellunker#$spellunker-%3Eadd\_stopwords(@stopwords)
 
 # LICENSE
 
--- lib/Code/TidyAll/Plugin/Spellunker.pm
+++ lib/Code/TidyAll/Plugin/Spellunker.pm
@@ -3,29 +3,84 @@ use 5.008001;
 use strict;
 use warnings;
 
-use Moo;
-
 our $VERSION = "0.01";
+
+use Moo;
 
 extends 'Code::TidyAll::Plugin';
 
 use Spellunker;
 
-sub validate_file {
-    my ($self, $file) = @_;
+use Specio::Declare;
+use Specio::Library::Builtins;
+
+has stopwords => (
+    is      => 'ro',
+    isa     => anon(
+        parent => t('Str'),
+        where  => sub { $_[0] =~ /^ \s* [^,]+ (?: \s* ,[^,]+ )* \s* $/xo },
+    ),
+    default => q{},
+);
+
+has parsed_stopwords => (
+    is  => 'lazy',
+    isa => t('ArrayRef'),
+);
+
+## no critic (ProhibitUnusedPrivateSubroutines)
+sub _build_parsed_stopwords {
+    my $self = shift;
+    return [split /\s*,\s*/x, $self->stopwords];
+}
+## use critic (ProhibitUnusedPrivateSubroutines)
+
+sub validate_source {
+    my ($self, $source) = @_;
+
+    my @errors = $self->_check_source($source);
+    return unless @errors;
+
+    my $msg = _stringify_errors(@errors);
+    die $msg; ## no critic (RequireCarping)
+}
+
+sub _check_source {
+    my ($self, $source) = @_;
 
     my $engine = Spellunker->new();
-
-    open my $fh, '<', $file
-        or die "$!: $file";
+    $engine->add_stopwords(@{ $self->parsed_stopwords });
 
     my @errors;
-    while (<$fh>) {
-        my @words = $engine->check_line($_);
-        push @errors, map +{ word => $_, line => $. }, @words if @words;
+
+    my $lineno = 0;
+    for my $line (split quotemeta $/, $source) {
+        $lineno++;
+
+        my @line_errors = $engine->check_line($line);
+        next unless @line_errors;
+
+        push @errors => [
+            $lineno,
+            $line,
+            \@line_errors,
+        ];
     }
 
-    die join $/, map "Bad: $_->{word} at $file line $_->{line}", @errors if @errors;
+    return @errors;
+}
+
+sub _stringify_errors {
+    my @errors = @_;
+
+    my $msg = "Errors:\n";
+    for (@errors) {
+        my ($lineno, $line, $errs) = @$_;
+        for my $err (@$errs) {
+            $msg .= "    $lineno: $err\n";
+        }
+    }
+    return $msg;
 }
 
 1;
@@ -35,15 +90,29 @@ __END__
 
 =head1 NAME
 
-Code::TidyAll::Plugin::Spellunker - It's new $module
+Code::TidyAll::Plugin::Spellunker - Code::TydyAll plugin for Spellunker
 
 =head1 SYNOPSIS
 
-    use Code::TidyAll::Plugin::Spellunker;
+    [Spellunker]
+    select = doc/**/*.txt
+    stopwords = karupanerura
+
+    [Spellunker::Pod]
+    select = lib/**/*.{pm,pod}
+    stopwords = karupanerura
 
 =head1 DESCRIPTION
 
-Code::TidyAll::Plugin::Spellunker is ...
+Code::TidyAll::Plugin::Spellunker is Code::TydyAll plugin for Spellunker.
+
+=head1 OPTIONS
+
+=head2 stopwords
+
+Add stopwords to the on memory dictionary. Separate it by ",".
+
+SEE ALSO: https://metacpan.org/pod/Spellunker#$spellunker-%3Eadd_stopwords(@stopwords)
 
 =head1 LICENSE
 
